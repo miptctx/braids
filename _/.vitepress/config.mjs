230a64b178b8c569dcd821b0d050be2ecfe22b80
Illia Rohozhkin <systemmind@ukr.net>
2026-07-20 14:54:51 +0300

    fix math module

--- _/.vitepress/config.mjs
+++ _/.vitepress/config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vitepress'
+import mathjax3 from 'markdown-it-mathjax3'
+
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Braids, knots and links calculation results",
   description: "This website contains calculations of braids, knots and links invariants",
+  markdown: {
+    config: (md) => {
+      md.use(mathjax3)
+    }
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
